01100626c28f30c70170eb6d767272d9fa307519
Camilo Lopez <>
2024-08-23 19:22:13 +0200

    Change from picamera to picamera2

--- camera_stream.py
+++ camera_stream.py
@@ -1,17 +1,16 @@
+import time
+from picamera2 import Picamera2
 import io
-import picamera
-from time import sleep
 
 def generate_frames():
-    with picamera.PiCamera() as camera:
-        # Inicializar la cámara
-        camera.resolution = (640, 480)
-        camera.framerate = 24
-        sleep(2)  # Dar tiempo a la cámara para inicializarse
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+    picam2.start()
+
+    while True:
         stream = io.BytesIO()
-        for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
-            stream.seek(0)
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
-            stream.seek(0)
-            stream.truncate()
+        picam2.capture_file(stream, format='jpeg')
+        stream.seek(0)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + stream.getvalue() + b'\r\n')
+        time.sleep(0.1)
